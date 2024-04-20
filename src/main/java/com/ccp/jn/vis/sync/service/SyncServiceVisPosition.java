7982f1240bca0ae1dcda95258b0daf36eb778929
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-19 23:56:40 -0300

    .

--- src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisPosition.java
+++ src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisPosition.java
@@ -8,7 +8,7 @@ import com.jn.vis.commons.utils.VisTopics;
 public class SyncServiceVisPosition {
 
 	public CcpJsonRepresentation save(String email,CcpJsonRepresentation json) {
-		CcpJsonRepresentation send = new CcpAsyncProcess().send(json.put("email", email), VisTopics.savePosition, new JnEntityAsyncTask());
+		CcpJsonRepresentation send = new CcpAsyncProcess().send(json.put("email", email), VisTopics.savePosition.name(), new JnEntityAsyncTask());
 		return send;
 	}
 	
--- src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
+++ src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
@@ -7,7 +7,7 @@ import com.jn.vis.commons.utils.VisTopics;
 
 public class SyncServiceVisResume {
 	public CcpJsonRepresentation save(String email, CcpJsonRepresentation json) {
-		CcpJsonRepresentation send = new CcpAsyncProcess().send(json.put("email", email), VisTopics.saveResume, new JnEntityAsyncTask());
+		CcpJsonRepresentation send = new CcpAsyncProcess().send(json.put("email", email), VisTopics.saveResume.name(), new JnEntityAsyncTask());
 		return send;
 	}
 
