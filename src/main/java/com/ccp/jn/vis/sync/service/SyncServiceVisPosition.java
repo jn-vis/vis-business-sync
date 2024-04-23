0a7220d224fa5140ca4618c10579bbe81a9bf760
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-23 10:13:04 -0300

    .

--- src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisPosition.java
+++ src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisPosition.java
@@ -1,16 +1,21 @@
 package com.ccp.jn.vis.sync.service;
 
+import java.util.Map;
+
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.jn.sync.business.utils.JnSyncMensageriaSender;
+import com.ccp.validation.CcpJsonFieldsValidations;
+import com.ccp.vis.sync.validations.JsonFieldsValidationsVisPosition;
 import com.jn.vis.commons.utils.VisTopics;
 
 public class SyncServiceVisPosition {
 
-	public CcpJsonRepresentation save(String email,CcpJsonRepresentation json) {
+	public Map<String, Object> save(String email, Map<String, Object> json) {
+		CcpJsonFieldsValidations.validate(JsonFieldsValidationsVisPosition.class, json);
+		CcpJsonRepresentation map = new CcpJsonRepresentation(json).put("email", email);
 		JnSyncMensageriaSender jnMensageria = new JnSyncMensageriaSender();
-		CcpJsonRepresentation put = json.put("email", email);
-		CcpJsonRepresentation send = jnMensageria.send(put, VisTopics.savePosition);
-		return send;
+		CcpJsonRepresentation send = jnMensageria.send(map, VisTopics.savePosition);
+		return send.content;
 	}
-	
+
 }
