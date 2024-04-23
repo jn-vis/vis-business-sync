da64d9fa6336537e613f885d566cdf9946e91076
J4550N51LV4 <jasson@noxxonsat.com.br>
2024-04-23 20:55:02 -0300

    Revisão de Classes

--- src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
+++ src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
@@ -10,16 +10,22 @@ import com.ccp.vis.sync.validations.JsonFieldsValidationsVisResume;
 import com.jn.vis.commons.utils.VisTopics;
 
 public class SyncServiceVisResume {
-	
+	// O Projeto contem business no nome porque trata de lógica de negócio, contem sync no nome por serem regras a 
+	// 	serem executadas ao vivo (imediatamente).
 	public Map<String, Object> save(String email, Map<String, Object> json) {
-
+		// A linha abaixo trata-se de validações do campo do json; se quiser saber quais regras são essas, 
+		// 	entre na classe que termina com .class no final.
 		CcpJsonFieldsValidations.validate(JsonFieldsValidationsVisResume.class, json);
+		// Dados vem na URL e dados vem no JSON, a linha abaixo junta os dois grupos de informação.
 		CcpJsonRepresentation resume = new CcpJsonRepresentation(json).put("email", email);
 		JnSyncMensageriaSender ccpAsyncProcess = new JnSyncMensageriaSender();
-
+		// As duas linhas abaixo colocam os dados para processar em processos paralelos (isso só é possível porque esses 
+		//  processos não têm interdependênica entre si).
 		CcpJsonRepresentation sendResultFromSaveResumeFile = ccpAsyncProcess.send(resume, VisTopics.saveResumeFile);
 		CcpJsonRepresentation sendResultFromSaveResume = ccpAsyncProcess.send(resume, VisTopics.saveResume);
-		
+		// Nas linhas abaixo é iniciado um JSON vazio que concatena os dados de feedback dos dois processos paralelos acima.
+		// Esses dados são importantes por se referirem a tickets que servirão para o frontend verificar status do processo
+		//	paralelo.
 		CcpJsonRepresentation put = CcpConstants.EMPTY_JSON
 				.put("saveResumeFile", sendResultFromSaveResumeFile)
 				.put("saveResume", sendResultFromSaveResume);
