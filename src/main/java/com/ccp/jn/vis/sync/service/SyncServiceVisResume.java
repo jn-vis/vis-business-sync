2bd706cc8cc293551dfb7cdd53e0cc2e0ceb76f8
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-26 22:49:57 -0300

    .

--- src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
+++ src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
@@ -8,7 +8,7 @@ import com.ccp.jn.sync.business.utils.JnSyncMensageriaSender;
 import com.ccp.validation.CcpJsonFieldsValidations;
 import com.ccp.vis.sync.validations.JsonFieldsValidationsVisResume;
 import com.jn.vis.commons.entities.VisEntityResume;
-import com.jn.vis.commons.utils.VisTopics;
+import com.jn.vis.commons.utils.VisAsyncBusiness;
 
 public class SyncServiceVisResume {
 	// O Projeto contem business no nome porque trata de lógica de negócio, contem sync no nome por serem regras a 
@@ -20,13 +20,12 @@ public class SyncServiceVisResume {
 		// Dados vem na URL e dados vem no JSON, a linha abaixo junta os dois grupos de informação.
 		CcpJsonRepresentation resume = new CcpJsonRepresentation(json).put("email", email);
 		//TODO caça as bruxas contra a palavra "new"
-		JnSyncMensageriaSender ccpAsyncProcess = new JnSyncMensageriaSender();
 		// As duas linhas abaixo colocam os dados para processar em processos paralelos (isso só é possível porque esses 
 		//  processos não têm interdependênica entre si).
-		CcpJsonRepresentation sendResultFromSaveResumeFile = ccpAsyncProcess.send(resume, VisTopics.saveResumeFile);
+		CcpJsonRepresentation sendResultFromSaveResumeFile = JnSyncMensageriaSender.INSTANCE.send(resume, VisAsyncBusiness.saveResumeFile);
 		// Vis.1.1
 		VisEntityResume.INSTANCE.createOrUpdate(resume);
-		CcpJsonRepresentation sendResultFromSaveResume = ccpAsyncProcess.send(resume, VisTopics.sendResumeToRecruiters);
+		CcpJsonRepresentation sendResultFromSaveResume = JnSyncMensageriaSender.INSTANCE.send(resume, VisAsyncBusiness.sendResumeToRecruiters);
 		// Nas linhas abaixo é iniciado um JSON vazio que concatena os dados de feedback dos dois processos paralelos acima.
 		// Esses dados são importantes por se referirem a tickets que servirão para o frontend verificar status do processo
 		//	paralelo.
