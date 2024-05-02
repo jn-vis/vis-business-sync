293cae70178157f81b45d43c27671a5e03ad3c68
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-01 23:02:44 -0300

    Refatoração do processo de login

--- src/main/java/com/ccp/vis/sync/validations/JsonFieldsValidationsVisPosition.java
+++ src/main/java/com/ccp/vis/sync/validations/JsonFieldsValidationsVisPosition.java
@@ -15,56 +15,48 @@ import com.ccp.validation.enums.SimpleArrayValidations;
 import com.ccp.validation.enums.SimpleObjectValidations;
 
 @ValidationRules(
-
 		simpleObject = {
 				@SimpleObject(rule = SimpleObjectValidations.requiredFields, fields = { "title", "description",
 						"contactChannel", "expireDate", "mandatorySkill", "desiredSkill", "ddd", "seniority",
-						"disponibility", "pcd", "sortFields", "frequency", "channel" }),
-				@SimpleObject(rule = SimpleObjectValidations.requiredAtLeastOne, fields = { "btc", "clt", "pj" }),
-				@SimpleObject(rule = SimpleObjectValidations.nonRepeatedLists, fields = { "ddd", "seniority",
-						"desiredSkill", "mandatorySkill", "sortFields", "channel" }),
+						"disponibility", "pcd", "sortFields", "frequency", "channel", "showSalaryExpectation" }),
+				@SimpleObject(rule = SimpleObjectValidations.requiredAtLeastOne, fields = { "maxClt", "maxPj" }),
+				@SimpleObject(rule = SimpleObjectValidations.requiredAtLeastOne, fields = { "minClt", "minPj" }),
+				@SimpleObject(rule = SimpleObjectValidations.nonRepeatedLists, fields = { "ddd", "channel" , 
+						"desiredSkill", "mandatorySkill", "sortFields"}),
 				@SimpleObject(rule = SimpleObjectValidations.integerFields, fields = { "disponibility" }),
-				@SimpleObject(rule = SimpleObjectValidations.booleanFields, fields = { "pcd" }),
-
+				@SimpleObject(rule = SimpleObjectValidations.booleanFields, fields = { "pcd", "showSalaryExpectation" }),
 		},
-
 		allowedValues = { @AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedNumbers, fields = {
 				"ddd" }, allowedValues = { "10", "61", "62", "64", "65", "66", "67", "82", "71", "73", "74", "75",
 						"77", "85", "88", "98", "99", "83", "81", "87", "86", "89", "84", "79", "68", "96", "92", "97",
 						"91", "93", "94", "69", "95", "63", "27", "28", "31", "32", "33", "34", "35", "37", "38", "21",
 						"22", "24", "11", "12", "13", "14", "15", "16", "17", "18", "19", "41", "42", "43", "44", "45",
 						"46", "51", "53", "54	", "55", "47", "48", "49" }),
-				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedTexts, fields = {
+				@AllowedValues(rule = AllowedValuesValidations.objectWithAllowedTexts, fields = {
 						"seniority" }, allowedValues = { "JR", "PL", "SR", "ES" }),
 				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedTexts, fields = {
 						"sortFields" }, allowedValues = { "seniority", "pj", "clt", "btc", "disponibility",
-								"desiredSkills", "reputation" }),
+								"desiredSkills"}),
 				@AllowedValues(rule = AllowedValuesValidations.objectWithAllowedTexts, fields = {
 						"frequency" }, allowedValues = { "minute", "hourly", "daily", "weekly", "monthly" }),
 				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedTexts, fields = {
 						"channel" }, allowedValues = { "telegram", "whatsapp", "email", "sms" }),
-
 		},
-
 		simpleArray = {
 
-				@SimpleArray(rule = SimpleArrayValidations.notEmptyArray, fields = { "ddd", "seniority",
-						"mandatorySkill", "sortFields" }),
+				@SimpleArray(rule = SimpleArrayValidations.notEmptyArray, fields = { "ddd", "mandatorySkill", "sortFields" }),
+				@SimpleArray(rule = SimpleArrayValidations.jsonItems, fields = { "desiredSkill" }),
 				@SimpleArray(rule = SimpleArrayValidations.integerItems, fields = { "ddd" }),
-				@SimpleArray(rule = SimpleArrayValidations.jsonItems, fields = { "desiredSkill" })
-
 		},
-
 		objectNumbers = {
 				@ObjectNumbers(rule = ObjectNumberValidations.equalsOrGreaterThan, bound = 0, fields = {
 						"disponibility" }),
 				@ObjectNumbers(rule = ObjectNumberValidations.equalsOrLessThan, bound = 365, fields = {
 						"disponibility" }),
-				@ObjectNumbers(rule = ObjectNumberValidations.equalsOrGreaterThan, bound = 1000, fields = { "btc",
-						"clt", "pj" }),
-				@ObjectNumbers(rule = ObjectNumberValidations.equalsOrLessThan, bound = 100000, fields = { "btc", "clt",
-						"pj" }) },
-
+				@ObjectNumbers(rule = ObjectNumberValidations.equalsOrGreaterThan, bound = 1000, fields = { "minBtc",
+						"minClt", "minPj" }),
+				@ObjectNumbers(rule = ObjectNumberValidations.equalsOrLessThan, bound = 100000, fields = { "maxBtc", "maxClt",
+						"maxPj" }) },
 		objectTextSize = {
 				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrGreaterThan, fields = { "title" }, bound = 3),
 				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrLessThan, fields = { "title" }, bound = 100),
@@ -76,9 +68,7 @@ import com.ccp.validation.enums.SimpleObjectValidations;
 						"contactChannel" }, bound = 3),
 				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrLessThan, fields = {
 						"contactChannel" }, bound = 100),
-
 		},
-
 		day = { @Day(rule = DayValidations.equalsOrGreaterThan, fields = { "expireDate" }, bound = 0),
 				@Day(rule = DayValidations.equalsOrLessThan, fields = { "expireDate" }, bound = 365), }
 
--- src/main/java/com/ccp/vis/sync/validations/JsonFieldsValidationsVisResume.java
+++ src/main/java/com/ccp/vis/sync/validations/JsonFieldsValidationsVisResume.java
@@ -17,27 +17,24 @@ import com.ccp.validation.enums.YearValidations;
 @ValidationRules(
 
 		simpleObject = {
-				@SimpleObject(rule = SimpleObjectValidations.requiredFields, fields = { "ddd",
-						"disabilities", "companiesNotAllowed", "disponibility", "observations", "resumeBase64",
-						"resumeWords", "resumeText", "desiredJob", "lastJob", "experience" }),
+				@SimpleObject(rule = SimpleObjectValidations.requiredFields, fields = { "ddd", "disabilities",
+						"companiesNotAllowed", "disponibility", "observations", "resumeBase64", "resumeWords",
+						"resumeText", "desiredJob", "lastJob", "experience" }),
 				@SimpleObject(rule = SimpleObjectValidations.requiredAtLeastOne, fields = { "btc", "clt", "pj" }),
 				@SimpleObject(rule = SimpleObjectValidations.nonRepeatedLists, fields = { "ddd", "disabilities",
 						"companiesNotAllowed", "resumeWords" }),
-				@SimpleObject(rule = SimpleObjectValidations.integerFields, fields = { "disponibility" }),
-				}
+				@SimpleObject(rule = SimpleObjectValidations.integerFields, fields = { "disponibility" }), }
 
 		, allowedValues = {
 
-				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedNumbers, fields = { "ddd" },
-
+				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedNumbers, fields = { "ddd" },//TODO COMO INTERNACIONALIZAR ISSO???
 						allowedValues = { "10", "61", "62", "64", "65", "66", "67", "82", "71", "73", "74", "75", "77",
 								"85", "88", "98", "99", "83", "81", "87", "86", "89", "84", "79", "68", "96", "92",
 								"97", "91", "93", "94", "69", "95", "63", "27", "28", "31", "32", "33", "34", "35",
 								"37", "38", "21", "22", "24", "11", "12", "13", "14", "15", "16", "17", "18", "19",
 								"41", "42", "43", "44", "45", "46", "51", "53", "54	", "55", "47", "48", "49" }),
 				@AllowedValues(rule = AllowedValuesValidations.arrayWithAllowedTexts, fields = { "disabilities" },
-
-						allowedValues = { "física/motora", "intelectual/mental", "visual", "auditiva" })
+						allowedValues = { "física/motora", "intelectual/mental", "visual", "auditiva" })//TODO COMO INTERNACIONALIZAR ISSO???
 
 		},
 
@@ -53,7 +50,15 @@ import com.ccp.validation.enums.YearValidations;
 				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrGreaterThan, fields = {
 						"resumeText" }, bound = 512),
 				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrLessThan, fields = { "desiredJob",
-						"lastJob" }, bound = 100) },
+						"lastJob" }, bound = 100),
+				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrGreaterThan, fields = {"name", "github"}, 
+				bound = 10), 
+				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrLessThan, fields = {"name"}, 
+				bound = 50), 
+				@ObjectTextSize(rule = ObjectTextSizeValidations.equalsOrLessThan, fields = {"github"}, 
+				bound = 200), 
+				
+		},
 
 		simpleArray = { @SimpleArray(rule = SimpleArrayValidations.notEmptyArray, fields = { "ddd", "resumeWords" }),
 				@SimpleArray(rule = SimpleArrayValidations.integerItems, fields = { "ddd" }), }
