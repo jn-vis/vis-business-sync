2fe7975cc8391272275c05040f7fec1478ece5bb
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-14 20:55:28 -0300

    .

--- src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
+++ src/main/java/com/ccp/jn/vis/sync/service/SyncServiceVisResume.java
@@ -1,37 +1,83 @@
 package com.ccp.jn.vis.sync.service;
 
-import java.util.Map;
-
 import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
+import com.ccp.dependency.injection.CcpDependencyInjection;
+import com.ccp.especifications.db.crud.CcpCrud;
+import com.ccp.especifications.db.crud.CcpSelectUnionAll;
+import com.ccp.especifications.db.utils.CcpEntity;
 import com.ccp.jn.sync.commons.JnSyncMensageriaSender;
+import com.ccp.jn.sync.service.SyncServiceJnLogin;
 import com.ccp.validation.CcpJsonFieldsValidations;
 import com.ccp.vis.sync.validations.JsonFieldsValidationsVisResume;
 import com.jn.vis.commons.entities.VisEntityResume;
 import com.jn.vis.commons.utils.VisAsyncBusiness;
 
 public class SyncServiceVisResume {
+	private final SyncServiceJnLogin loginService = new SyncServiceJnLogin();
 
-	public Map<String, Object> save(CcpJsonRepresentation resume) {
+	public CcpJsonRepresentation save(CcpJsonRepresentation resume) {
 
 		CcpJsonFieldsValidations.validate(JsonFieldsValidationsVisResume.class, resume.content, "saveResume");
+	
+		this.loginService.validateSession(resume);
+
+		CcpJsonRepresentation sendResultFromSaveResume = JnSyncMensageriaSender.INSTANCE.send(resume, VisAsyncBusiness.sendResumeToRecruiters);
 
 		CcpJsonRepresentation sendResultFromSaveResumeFile = JnSyncMensageriaSender.INSTANCE.send(resume, VisAsyncBusiness.saveResumeFile);
 
-		VisEntityResume.INSTANCE.createOrUpdate(resume);
-		CcpJsonRepresentation sendResultFromSaveResume = JnSyncMensageriaSender.INSTANCE.send(resume, VisAsyncBusiness.sendResumeToRecruiters);
 
 		CcpJsonRepresentation put = CcpConstants.EMPTY_JSON
 				.put("sendResumeToRecruiters", sendResultFromSaveResume)
 				.put("saveResumeFile", sendResultFromSaveResumeFile)
 				;
 		
-		return  put.content;
+		return  put;
 	}
 
-	public Map<String, Object> delete(CcpJsonRepresentation sessionValues) {
-		// TODO Auto-generated method stub
-		return null;
+	public CcpJsonRepresentation delete(CcpJsonRepresentation sessionValues) {
+
+		this.loginService.validateSession(sessionValues);
+		
+		CcpJsonRepresentation deleteResume = JnSyncMensageriaSender.INSTANCE.send(sessionValues, VisAsyncBusiness.deleteResume);
+
+		CcpJsonRepresentation put = CcpConstants.EMPTY_JSON.put("deleteResume", deleteResume);
+
+		return put;
+	}
+
+	public CcpJsonRepresentation changeStatus(CcpJsonRepresentation sessionValues) {
+
+		this.loginService.validateSession(sessionValues);
+		
+		CcpJsonRepresentation changeResumeStatus = JnSyncMensageriaSender.INSTANCE.send(sessionValues, VisAsyncBusiness.changeResumeStatus);
+
+		CcpJsonRepresentation put = CcpConstants.EMPTY_JSON.put("changeResumeStatus", changeResumeStatus);
+
+		return put;
+	}
+	
+	
+	public CcpJsonRepresentation getResume(CcpJsonRepresentation sessionValues) {
+		
+		this.loginService.validateSession(sessionValues);
+
+		CcpCrud crud = CcpDependencyInjection.getDependency(CcpCrud.class);
+		
+		CcpEntity mirrorEntity = VisEntityResume.INSTANCE.getMirrorEntity();
+		CcpSelectUnionAll unionAll = crud.unionAll(sessionValues, VisEntityResume.INSTANCE, mirrorEntity);
+		
+		boolean activeResume = unionAll.isPresent(VisEntityResume.INSTANCE, sessionValues);
+		
+		if(activeResume) {
+			CcpJsonRepresentation requiredEntityRow = unionAll.getRequiredEntityRow(VisEntityResume.INSTANCE, sessionValues);
+			CcpJsonRepresentation put = requiredEntityRow.put("activeResume", true);
+			return put;
+		}
+		
+		CcpJsonRepresentation requiredEntityRow = unionAll.getRequiredEntityRow(mirrorEntity, sessionValues);
+		CcpJsonRepresentation put = requiredEntityRow.put("activeResume", false);
+		return put;
 	}
 
 }
